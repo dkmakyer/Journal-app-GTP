0f129ada9a566f5ceac5089b7cdc0bc4d59ff1a7
dkmakyer <kwasiakyer2105@gmail.com>
2025-05-07 10:55:29 +0000

    implement functionality to save and retrieve from local storage

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,18 +1,47 @@
+const journalEntryModal = document.getElementById("journal-entry-modal");
+const journalDetailContainer = document.getElementById("journal-detail-container");
+const mainContainer = document.getElementById("main-container");
+const journalEntryForm = document.getElementById("journal-entry-form");
 
-export function renderUI(){
-    const journalEntryModalWrapper = document.getElementById("journal-entry-modal-wrapper");
-    const journalDetailContainerWrapper = document.getElementById("journal-detail-container-wrapper");
-    document.getElementById("back-to-list-button").addEventListener("click", () => {
-        journalDetailContainerWrapper.classList.add("hidden");
-    });
-    
-    document.getElementById("journal-display-button").addEventListener("click", () => {
-        journalEntryModalWrapper.classList.remove("hidden");
-    });
-    
-    document.getElementById("hide-journal-entry-modal").addEventListener("click", () => {
-        journalEntryModalWrapper.classList.add("hidden");
-    });
-}
 
-document.addEventListener("DOMContentLoaded", () => renderUI());
+//on the first render, retrieve an existing journal from local storage, or initialize an empty array
+const journalArray = JSON.parse(localStorage.getItem("journalArray")) || [];
+
+//extract the form data at once
+journalEntryForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(journalEntryForm);
+    const journalEntry = Object.fromEntries(formData.entries());
+    let timeCreated = Date.now();
+
+    journalEntry.id = timeCreated.toString();
+
+    journalArray.push(journalEntry);
+    //store the entire array
+    localStorage.setItem("journalArray", JSON.stringify(journalArray));
+    
+    //store the individual entry for faster search
+    localStorage.setItem(`entry_${timeCreated}`, JSON.stringify(journalEntry));//saved them with the time created to make the key in the local storage unique
+    
+    journalEntryForm.reset();
+    journalEntryModal.classList.add("hidden");
+});
+
+document.getElementById("back-to-list-button").addEventListener("click", () => {
+    journalDetailContainer.classList.add("hidden");
+});
+
+document.getElementById("journal-display-button").addEventListener("click", () => {
+    journalEntryModal.classList.remove("hidden");
+});
+
+document.getElementById("hide-journal-entry-modal").addEventListener("click", () => {
+    journalEntryModal.classList.add("hidden");
+});
+
+// export function renderUI(){
+
+// }
+
+// document.addEventListener("DOMContentLoaded", () => renderUI());
